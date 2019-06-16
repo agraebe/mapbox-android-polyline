fa1ef03257fce81f99a09d72d50bd137aa06fbdd
Alexander Graebe <alex.graebe@gmail.com>
2019-06-16 17:37:58 +0200

    Initial commit

--- tutorials/Polylines/app/src/main/java/com/example/polyline/PolyActivity.java
+++ tutorials/Polylines/app/src/main/java/com/example/polyline/PolyActivity.java
@@ -1,68 +1,117 @@
 package com.example.polyline;
 
-import android.graphics.Color;
-import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.graphics.Color;
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.maps.Style;
+import com.mapbox.geojson.Point;
+import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Polyline;
-import com.google.android.gms.maps.model.PolylineOptions;
+import com.mapbox.geojson.Feature;
+import com.mapbox.geojson.FeatureCollection;
+import com.mapbox.geojson.LineString;
+import com.mapbox.mapboxsdk.style.layers.LineLayer;
+import com.mapbox.mapboxsdk.style.layers.PropertyFactory;
 
-import static com.example.polyline.R.id.map;
+import java.util.ArrayList;
+import java.util.List;
 
-
-/**
- * An activity that displays a Google map with polylines to represent paths or routes
- */
-public class PolyActivity extends AppCompatActivity
-        implements
-                OnMapReadyCallback {
+public class PolyActivity extends AppCompatActivity {
+    private MapView mapView;
+    private List<Point> routeCoordinates;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Retrieve the content view that renders the map.
+        Mapbox.getInstance(this, "pk.eyJ1IjoiYWdyYWViZSIsImEiOiJjand2MmNtc2wwcGR6M3lxejk1aDd0amo3In0.LneM6wuauNA7ArbZVn__2w");
         setContentView(R.layout.activity_maps);
+        mapView = findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(@NonNull MapboxMap mapboxMap) {
+                mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
+                    @Override
+                    public void onStyleLoaded(@NonNull Style style) {
 
-        // Get the SupportMapFragment and request notification when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(map);
-        mapFragment.getMapAsync(this);
+                        initRouteCoordinates();
+
+                        // Create the LineString from the list of coordinates and then make a GeoJSON
+                        // FeatureCollection so we can add the line to our map as a layer
+                        style.addSource(new GeoJsonSource("line-source",
+                                FeatureCollection.fromFeatures(new Feature[] {Feature.fromGeometry(
+                                        LineString.fromLngLats(routeCoordinates)
+                                )})));
+
+                        // The layer properties for our line
+                        style.addLayer(new LineLayer("linelayer", "line-source").withProperties(
+                                PropertyFactory.lineWidth(3f),
+                                PropertyFactory.lineColor(Color.parseColor("#FF0000"))
+                        ));
+                    }
+                });
+            }
+        });
     }
 
-    /**
-     * Manipulates the map when it's available.
-     * The API invokes this callback when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera.
-     */
+    private void initRouteCoordinates() {
+        routeCoordinates = new ArrayList<>();
+        routeCoordinates.add(Point.fromLngLat(-77.0381498336792,38.893596444352134));
+        routeCoordinates.add(Point.fromLngLat(-77.03792452812195,38.89337933372204));
+        routeCoordinates.add(Point.fromLngLat(-77.03761339187622, 38.89316222242831));
+        routeCoordinates.add(Point.fromLngLat(-77.03731298446655, 38.893028615148424));
+        routeCoordinates.add(Point.fromLngLat(-77.03691601753235, 38.892920059048464));
+        routeCoordinates.add(Point.fromLngLat(-77.03637957572937, 38.892903358095296));
+        routeCoordinates.add(Point.fromLngLat(-77.03592896461487, 38.89301191422077));
+        routeCoordinates.add(Point.fromLngLat(-77.03549981117249, 38.89316222242831));
+        routeCoordinates.add(Point.fromLngLat(-77.03514575958252, 38.89340438498248));
+        routeCoordinates.add(Point.fromLngLat(-77.0349633693695, 38.893596444352134));
+    }
+
     @Override
-    public void onMapReady(GoogleMap googleMap) {
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
 
-        Polyline path = googleMap.addPolyline(new PolylineOptions()
-                .add(
-                        new LatLng(38.893596444352134, -77.0381498336792),
-                        new LatLng(38.89337933372204, -77.03792452812195),
-                        new LatLng(38.89316222242831, -77.03761339187622),
-                        new LatLng(38.893028615148424, -77.03731298446655),
-                        new LatLng(38.892920059048464, -77.03691601753235),
-                        new LatLng(38.892903358095296, -77.03637957572937),
-                        new LatLng(38.89301191422077, -77.03592896461487),
-                        new LatLng(38.89316222242831, -77.03549981117249),
-                        new LatLng(38.89340438498248, -77.03514575958252),
-                        new LatLng(38.893596444352134, -77.0349633693695)
-                )
-        );
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
 
-        // Style the polyline
-        path.setWidth(10);
-        path.setColor(Color.parseColor("#FF0000"));
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
 
-        // Position the map's camera
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(38.89399, -77.03659), 16));
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
     }
 }
